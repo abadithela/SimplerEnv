ad09b342cb4e6c2527bbe3b0d793e0e0759bdae8
Apurva S. Badithela <ab5832@neuronic.cs.princeton.edu>
2024-09-05 09:38:12 -0400

    update scripts

--- scripts/rt1_coke_slurm.sh
+++ scripts/rt1_coke_slurm.sh
@@ -12,31 +12,32 @@ urdf_version=$2 # URDF version
 declare -a coke_can_options_arr=("lr_switch=True" "upright=True" "laid_vertically=True")
 
 # Setting a denser grid of obj x and y evals.
-do CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static \
-  --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
-  --env-name ${env_name} --scene-name ${scene_name} \
-  --rgb-overlay-path ${rgb_overlay_path} \
-  --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.355 -0.115 $1 50 --obj-init-y -0.025 0.500 20 \
-  --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
-  --additional-env-build-kwargs ${coke_can_options_arr[0]} urdf_version=${urdf_version} &
+CUDA_VISIBLE_DEVICES=${gpu_id} 
+python simpler_env/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
+--robot google_robot_static \
+--control-freq 3 --sim-freq 513 --max-episode-steps 80 \
+--env-name ${env_name} --scene-name ${scene_name} \
+--rgb-overlay-path ${rgb_overlay_path} \
+--robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.355 -0.115  50 --obj-init-y -0.025 0.500 20 \
+--robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
+--additional-env-build-kwargs ${coke_can_options_arr[0]} urdf_version=${urdf_version} &
 
-do CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static \
-  --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
-  --env-name ${env_name} --scene-name ${scene_name} \
-  --rgb-overlay-path ${rgb_overlay_path} \
-  --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.355 -0.115 $1 50 --obj-init-y -0.025 0.500 20 \
-  --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
-  --additional-env-build-kwargs ${coke_can_options_arr[1]} urdf_version=${urdf_version} &
+python simpler_env/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
+--robot google_robot_static \
+--control-freq 3 --sim-freq 513 --max-episode-steps 80 \
+--env-name ${env_name} --scene-name ${scene_name} \
+--rgb-overlay-path ${rgb_overlay_path} \
+--robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.355 -0.115 50 --obj-init-y -0.025 0.500 20 \
+--robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
+--additional-env-build-kwargs ${coke_can_options_arr[1]} urdf_version=${urdf_version} &
 
-do CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
-  --robot google_robot_static \
-  --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
-  --env-name ${env_name} --scene-name ${scene_name} \
-  --rgb-overlay-path ${rgb_overlay_path} \
-  --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.355 -0.115 $1 50 --obj-init-y -0.025 0.500 20 \
-  --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
-  --additional-env-build-kwargs ${coke_can_options_arr[2]} urdf_version=${urdf_version} &
+python simpler_env/main_inference.py --policy-model rt1 --ckpt-path ${ckpt_path} \
+--robot google_robot_static \
+--control-freq 3 --sim-freq 513 --max-episode-steps 80 \
+--env-name ${env_name} --scene-name ${scene_name} \
+--rgb-overlay-path ${rgb_overlay_path} \
+--robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.355 -0.115 50 --obj-init-y -0.025 0.500 20 \
+--robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
+--additional-env-build-kwargs ${coke_can_options_arr[2]} urdf_version=${urdf_version} 
 
 
